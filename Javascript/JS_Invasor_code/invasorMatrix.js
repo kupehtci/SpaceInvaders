2a3fda3e176110296255857c3acd4fc1b10397b3
kupehtci <daniel.laplana.gimeno@gmail.com>
2023-01-09 15:44:46 +0100

    Ubicar invasores actualizado

--- Javascript/JS_Invasor_code/invasorMatrix.js
+++ Javascript/JS_Invasor_code/invasorMatrix.js
@@ -6,17 +6,28 @@ class InvasorMatrix{
      * When instantiated this matrix create as much aliens as the columns x rows defined
      * @param {int} numColumns Number of columns in the matrix of aliens
      * @param {int} numRows Number of rows in the matrix of aliens
-     * @param {int} screenWidth 
-     * @param {int} screenHeight 
+     * @param {int} screenWidth Width of the screen for calculating aliens position
+     * @param {int} screenHeight Height of the screen for calculating aliens position and checking when invasors has reached earth and loose game
      */
     constructor(numColumns,numRows,screenWidth,screenHeight){
         if(typeof(numColumns) != "number"){console.log("Incorrect number of columns")}
         
+        //Position vars
+        this.sepLeft = 20; 
+        this.sepTop = 20; 
+        this.sepWide = 30; 
+        this.sepTall = 50; 
+
         //Create all the invasors
         this.invasors = [];
         for(var i = 0; i < numRows; i++){
             for(var j = 0; j < numColumns; i++){
-                this.invasors[numColumns * i + j] = new Alien(); 
+                let index = (numColumns * i + j); 
+                this.invasors[index] = new Alien();
+
+                let invasorPosX = this.sepLeft + (j * this.sepWide); 
+                let invasorPosY = this.sepHeight + (i * this.sepTall);
+                this.invasors[index].SetPosition(invasorPosX,invasorPosY); 
             }
         }
         
